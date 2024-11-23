6fcd4ee2f2c7aaca1dd7da9be8002ec975c2c66b
Pybounce <pybounce@protonmail.com>
2024-11-23 05:44:02 +0000

    position tests

---
+++ tests/unit_tests/PositionTests.cs
@@ -0,0 +1,37 @@
+using Bogus;
+
+namespace unit_tests;
+
+public class PositionTests
+{
+    private Faker _faker;
+
+    [SetUp]
+    public void Setup()
+    {
+        _faker = new Faker();
+    }
+
+    [Test]
+    [Repeat(20)]
+    public void AsDrawnString___RandomInput___StringContainsSameValues()
+    {
+        var x = _faker.Random.Int();
+        var y = _faker.Random.Int();
+        var expectedOutput = $"({x}, {y})";
+
+        var position = new Position(x, y);
+    
+        Assert.That(expectedOutput == position.AsDrawnString());
+    }
+
+    [Test]
+    public void AsDrawnString___NoInput___StringContainsSameValues()
+    {
+        var expectedOutput = "(0, 0)";
+
+        var position = new Position();
+        
+        Assert.That(expectedOutput == position.AsDrawnString());
+    }
+}
